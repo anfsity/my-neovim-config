2a351836ed40c67a8939409595669f3ec775878b
Anfsity <186920685+anfsity@users.noreply.github.com>
2025-09-03 03:26:01 +0800

    add personal configuration about keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,28 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("i", "<A-h>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<A-l>", "<End>", { desc = "move end of line" })
+map("n", "F", "%", { desc = "jump between match-pair" })
+
+
+-- use windows like keymaps 
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "file save" })
+map({ "n" }, "<C-a>", "ggVG", { desc = "select all file" })
+map({ "n", "i", "v" }, "<C-z>", "<cmd> undo <cr>", { desc = "history undo" })
+map({ "n", "i", "v" }, "<C-y>", "<cmd> redo <cr>", { desc = "history redo" })
+map("n", "<C-/>", "gcc", { desc = "comment toggle", remap = true })
+map("i", "<C-/>", "<Esc>gcc^i", { desc = "comment toggle", remap = true })
+
+
+-- visual studio code like keymaps
+
+map("n", "gb", "<C-o>", { desc = "jump back" })
+map("n", "gh", "<cmd> lua vim.lsp.buf.hover() <cr>", { desc = "LSP hover" })map("n", "ge", "<cmd> lua vim.diagnostic.open_float() <cr>", { desc = "LSP show diagonstics" })
+map("n", "ge", "<cmd> lua vim.diagnostic.open_float() <cr>", { desc = "LSP show diagonstics" })
+
+map({ "n", "i" }, "<A-j>", "<cmd> :m +1 <cr>", { desc = "move one line down "})
+map({ "n", "i" }, "<A-k>", "<cmd> :m -2 <cr>", { desc = "move one line up "})
