3f895d38b42584e0f5f2ab1a83d25a0721a6d060
Anfsity <186920685+anfsity@users.noreply.github.com>
2025-09-11 17:12:03 +0800

    fail to add vim-viusal-mutli plugin

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,2 +1,12 @@
 --- @type LazySpec
-return {}
+return {
+	-- {
+	-- 	"mg979/vim-visual-multi",
+	-- 	lazy = false,
+	-- 	config = function ()
+	-- 		vim.g.VM_maps = {
+	-- 			["Find Under"] = "<C-h>"
+	-- 		}
+	-- 	end,
+	-- },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,21 +1,21 @@
 --- @type LazySpec
 return {
-    {
-        "nvim-tree/nvim-tree.lua",
-        opts = require("configs.ui").nvim_tree,
-    },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = require("configs.ui").nvim_tree,
+	},
 
-    { -- Breadcrumbs
-        "Bekaboo/dropbar.nvim",
-        lazy = false,
-        -- Optional, but required for fuzzy finder support
-        dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim",
-        },
-    },
+	{ -- Breadcrumbs
+		"Bekaboo/dropbar.nvim",
+		lazy = false,
+		-- Optional, but required for fuzzy finder support
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+	},
 
-    -- { -- Set consistent terminal background color with theme
-    --     "typicode/bg.nvim",
-    --     lazy = false,
-    -- },
+	-- { -- Set consistent terminal background color with theme
+	--     "typicode/bg.nvim",
+	--     lazy = false,
+	-- },
 }
